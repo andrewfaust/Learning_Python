06baeeb136988c9b39ff1b8cb0deec08f1ddaa74
Andrew <mardeev@mail.ru>
2023-02-26 01:11:47 +1000

    Среднне мода медиана

---
+++ Задание6.py
@@ -0,0 +1,29 @@
+import numpy as np
+import pandas as pd
+from pandas import Series, DataFrame
+
+
+list = [5, 4, 6, 2, 1, 1, 5, 2, 5, 9, 7]
+
+numbers = pd.Series(list)
+Mean = numbers.mean()    #Среднее значение
+Median = numbers.median()   #Медиана
+Mode = numbers.mode()[0]   # index 1 gives the next mode (ie, 2)  Мода значение
+print(Mean, Median, Mode, sep = '\n') # sep = '\n' вывод по вертикали
+print(max(list)-min(list)) #Размах
+
+
+#Осталось найти дисперсию 6.61 и стандартное отклонение 2.57
+
+
+
+
+
+
+#list_avg = np.average(list) #Среднее значение
+#list_mediana = np.median(list) #Медиана
+#print(round(list_avg, 2))
+#print(list_mediana)
+
+
+
